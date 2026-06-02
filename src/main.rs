b68fd0db280d1eae4563412743a051cf1f55587a
howenyap <wenhyap02@gmail.com>
2026-06-02 19:30:08 +0800

    2.2 Creating an Image File

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let image_width = 256;
+    let image_height = 256;
+
+    println!("P3\n{image_width} {image_height}\n255");
+
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
+            let b = 0.0f64;
+
+            let ir = (255.99 * r) as i32;
+            let ig = (255.99 * g) as i32;
+            let ib = (255.99 * b) as i32;
+
+            println!("{ir} {ig} {ib}")
+        }
+    }
+}
