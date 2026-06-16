84406a5c619116dad28b61b0ee37f5ca28365d73
howenyap <wenhyap02@gmail.com>
2026-06-16 20:30:48 +0800

    10.5. A Scence with Metal Spheres

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,19 @@ fn main() {
 
     let camera = Camera::new(aspect_ratio, image_width, samples_per_pixel, max_depth);
 
+    let material_ground = Material::lambertian(Colour::new(0.8, 0.8, 0));
+    let material_center = Material::lambertian(Colour::new(0.1, 0.2, 0.5));
+    let material_left = Material::metal(Colour::new(0.8, 0.8, 0.8));
+    let material_right = Material::metal(Colour::new(0.8, 0.6, 0.2));
+
     let world = HittableList::new([
         Object::new(
-            Shape::sphere(Point::new(0, 0, -1), 0.5),
-            Material::lambertian(Colour::white()),
-        ),
-        Object::new(
             Shape::sphere(Point::new(0, -100.5, -1), 100),
-            Material::lambertian(Colour::white()),
+            material_ground,
         ),
+        Object::new(Shape::sphere(Point::new(0, 0, -1.2), 0.5), material_center),
+        Object::new(Shape::sphere(Point::new(-1, 0, -1), 0.5), material_left),
+        Object::new(Shape::sphere(Point::new(1, 0, -1), 0.5), material_right),
     ]);
 
     camera.render(&world);
